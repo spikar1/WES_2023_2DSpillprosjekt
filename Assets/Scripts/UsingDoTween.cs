0e9ecc5f2daa89735a4837cfd6a6ef985a1f3162
Steffen <steffen.fureli@gmail.com>
2023-01-08 12:13:06 +0100

    Update DoTween example and script

--- Assets/Scripts/UsingDoTween.cs
+++ Assets/Scripts/UsingDoTween.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 
 public class UsingDoTween : MonoBehaviour
 {
+    [SerializeField]
+    List<Rigidbody2D> objectsToMove = new List<Rigidbody2D>();
+
     // Start is called before the first frame update
     void Start()
     {
         transform.DOJump(Vector3.zero, 5, 2, 1).SetDelay(1);
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (int i = 0; i < objectsToMove.Count; i++)
+        {
+            Rigidbody2D obj = objectsToMove[i];
+            obj.DOMoveY(4, 1).SetEase(Ease.InOutSine).SetRelative(true).SetDelay((float)i * .2f).SetLoops(-1, LoopType.Yoyo);
+        }
+
     }
 }
